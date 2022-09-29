b2bd42f06242c3b495e5dc0e6a1c0d0aaf89debc
saimoomkabirsangram <saimoomkabirsangram@gmail.com>
2022-09-29 07:21:10 +0600

    Component Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Body from './Components/BodyContainer/Body';
 
 function App() {
   return (
     <div className="App">
-      <h1>Project Starts</h1>
+      <Body></Body>      
     </div>
   );
 }
