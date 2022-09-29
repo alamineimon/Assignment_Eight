405fe7ae4c4299e41623ab6ea70290e9c13d3acb
saimoomkabirsangram <saimoomkabirsangram@gmail.com>
2022-09-29 18:33:06 +0600

    Exercise and break section added

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,30 +1,24 @@
-import React from 'react';
+// import React from 'react';
 import './Activity.css';
-// import {toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import axios from 'axios';
-// import '../Activity/Activity.css';
+import React, { useState } from 'react';
+
 
 const Activity = ({singleProduct}) => {
     let totalTime = 0 ;
     for(const product of singleProduct){
-        totalTime = parseInt(totalTime + product.time) ;
+        totalTime =totalTime +  parseInt(product.time) ;
+    }
+    const [getTime , setGetTime] = useState([])
+    const TimeSet =(e)=>{
+        setGetTime(e.target.innerText);
     }
 
-    // for toast part 
-    // const successToast =()=>{
-    //     toast('Success You Are Done', {
-    //         className: 'custom-toast',
-    //         draggable: true,
-    //         position: toast.POSITION.TOP_CENTER
-    //     })
-    // }
     return (
         <div className='activity-container'>
             <div className=''>
                 <h1>Al Amin Eimon </h1>
             </div>
-                <div className="my-info">
+                <div className="info">
                     <div>
                         <h1>65(kg)</h1>
                         <p>Weight</p>
@@ -42,21 +36,21 @@ const Activity = ({singleProduct}) => {
 
                 <div className="take-time">
                     <ul>
-                        <li>10s</li>
-                        <li>20s</li>
-                        <li>30s</li>
-                        <li>40s</li>
-                        <li>50s</li>
+                        <button onClick={TimeSet} > 10s</button>
+                        <button onClick={TimeSet} >25s</button>
+                        <button onClick={TimeSet} >30s</button> 
+                        <button onClick={TimeSet} >45s</button>
+                        <button onClick={TimeSet} >50s</button>
                     </ul>
                 </div>
                 <h2>Exercise Details</h2>
-                <div className='exercise-times'>
-                    <h4>Exercise time: {totalTime}s</h4>
+                <div className='exercise-time'>
+                    <h4>Exercise Time: {totalTime}s</h4>
                 </div>
                 <div className='break-time'>
-                    <h4>Break time: </h4>
+                    <h4>Break Time:  {getTime}</h4>
                 </div>
-                <button  className='activity-btn'>
+                <button  className='activity-btnn'>
                     Completed Activity 
                 </button>
         </div>
